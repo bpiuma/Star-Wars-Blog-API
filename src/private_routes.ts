7c4b17c63a6baa3a5140e168a1770020d45a104f
Bernardo Piuma <bpiuma.uy@gmail.com>
2021-05-26 19:48:56 +0000

    Rutas finalizadas

--- src/private_routes.ts
+++ src/private_routes.ts
@@ -32,11 +32,15 @@ const verifyToken= (req: Request,res:Response, next:NextFunction) =>{
     next()
 }
 
-router.get('/user', verifyToken, safe(actions.getUsers));
+router.get('/user', safe(actions.getUsers));
 router.post('/characters', safe(actions.createCharacter));
 router.get('/character/:id', safe(actions.getCharacter));
 router.get('/planet/:id', safe(actions.getPlanet));
 router.post('/planets', safe(actions.createPlanet));
 router.post('/favCharacter/:id', safe(actions.FavCharacters));
+router.post('/favPlanet/:id', safe(actions.FavPlanets));
+router.delete('/favCharacter/:id', safe(actions.deleteFavCharacter));
+router.delete('/favPlanet/:id', safe(actions.deleteFavPlanet));
+router.get('/userFav', safe(actions.getUsersFav));
 
 export default router;
